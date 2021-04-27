e84770668e74cf5a2e19ee50efa729126adbc300
KNSM <knsmrt5k@gmail.com>
2021-04-27 17:17:21 +0300

    контент на главной

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,11 @@
 $(document).ready(function () {
+    var ov = $('.ov');
+
+    //fakeScroll
+    var fakeScrolls = document.querySelectorAll('.selector-menu'), fakeScroll;
+    for (fakeScroll = 0; fakeScroll < fakeScrolls.length; fakeScroll++) {
+        fakeScrolls[fakeScroll].fakeScroll();
+    }
 
     //header menu opener
     $(function () {
@@ -98,5 +105,70 @@ $(document).ready(function () {
                 });
             });
         }
+    });
+
+    //selector
+    $(function () {
+        var selector = $('.selector');
+
+        selector.each(function () {
+
+            var select = $(this).find('.select'),
+                selectorMenu = $(this).find('.selector-menu'),
+                selectorMenuInner = selectorMenu.find('.menu'),
+                selectItems = select.find('option');
+
+            $(this).append('<div class="selector-block"><button type="button" class="button"></button></div>');
+            select.wrap('<div class="form-data"></div>');
+
+
+            var selectorBlock = $(this).find('.selector-block'),
+                selectorButton = selectorBlock.find('.button');
+
+            selectItems.each(function () {
+
+                if ($(this).attr('selected')) {
+                    selectorMenuInner.append('<li data-id="' + $(this).attr('value') + '" class="selector-menu__item selected">' + $(this).html() + '</li>');
+
+                    var selectorMenuItem = selectorMenuInner.find('.selector-menu__item.selected');
+
+                    selectorButton.html($(this).html());
+                    selectorButton.attr('data-id', selectorMenuItem.attr('data-id'));
+                } else {
+                    selectorMenuInner.append('<li data-id="' + $(this).attr('value') + '" class="selector-menu__item">' + $(this).html() + '</li>');
+                }
+            });
+
+            var selectorMenuItem = selectorMenuInner.find('.selector-menu__item');
+
+            selectorButton.click(function () {
+                var menuLength = selectorMenuInner.find('.selector-menu__item').length;
+
+                if (menuLength < 5) {
+                    selectorMenu.css('height', 42 * menuLength + 12);
+                }
+
+                selectorMenu.toggleClass('-open');
+            });
+
+            selectorMenuItem.click(function () {
+                var dataId = $(this).attr('data-id'),
+                    selectorText = $(this).html();
+
+                selectItems.removeAttr('selected');
+
+                selectItems.each(function () {
+                    if ($(this)[0].value === dataId) {
+                        $(this).attr('selected', 'selected');
+                    }
+                });
+                selectItems.find('[value]', dataId);
+                selectorButton.html(selectorText);
+                selectorButton.attr('data-id', dataId);
+                selectorMenuItem.removeClass('selected');
+                $(this).addClass('selected');
+                selectorMenu.removeClass('-open');
+            });
+        });
     });
 });
